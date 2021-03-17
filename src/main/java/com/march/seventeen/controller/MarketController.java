ba08054bb490a9d78da3c75422fecb2fcae825f7
LEEJISANG <leejisang314@naver.com>
2021-03-17 13:20:10 +0900

    commit

---
+++ src/main/java/com/march/seventeen/controller/MarketController.java
@@ -0,0 +1,15 @@
+package com.march.seventeen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MarketController {
+
+	@RequestMapping(value = { "/", "index" }, method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>중고나라</title>
+</head>
+<body>
+	인덱스
+</body>
+</html>
